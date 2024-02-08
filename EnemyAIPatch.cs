16d6f23ffd5b4bdddee0f693a0d2f4d54639a095
Waffleburgers <159414455+Waffleburgers@users.noreply.github.com>
2024-02-07 23:04:42 -0800

    Add files via upload

---
+++ EnemyAIPatch.cs
@@ -0,0 +1,53 @@
+﻿using HarmonyLib;
+using JetBrains.Annotations;
+using System.ComponentModel;
+using System.Linq;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace MasksDropShells.Patches
+{
+    [HarmonyPatch(typeof(MaskedPlayerEnemy))]
+    internal class MaskedPlayerEnemy_Patches
+    {
+        [HarmonyPatch(nameof(MaskedPlayerEnemy.KillEnemy))]
+        [HarmonyPostfix]
+        internal static void DropMaskOnDeath(MaskedPlayerEnemy __instance)
+        {
+            MaskDropShell.Instance.mls.LogInfo("HarmonyPatchOpened");
+            if (!NetworkManager.Singleton.IsServer) return;
+
+            var shellPrefab = GetShellItem();
+
+            int shellRolls = MaskDropShell.Instance.ShellRolls.Value;
+            int shellChance = MaskDropShell.Instance.ShellChance.Value;
+            MaskDropShell.Instance.mls.LogInfo("masked dead");
+            for (int i = 0; i < shellRolls; i++)
+            {
+                MaskDropShell.Instance.mls.LogInfo("rolling");
+                int rnd = UnityEngine.Random.Range(0, 100);
+                MaskDropShell.Instance.mls.LogInfo(rnd);
+                if (rnd < shellChance)
+                {
+                    var shellObj = GameObject.Instantiate(shellPrefab.spawnPrefab, __instance.transform.position + new Vector3(0, 2.5f, 0), Quaternion.identity);
+            
+                    shellObj.GetComponentInChildren<GrabbableObject>().fallTime = 0f;
+                    shellObj.GetComponentInChildren<GrabbableObject>().SetScrapValue(0);
+                    shellObj.GetComponentInChildren<NetworkObject>().Spawn();
+                    RoundManager.Instance.SyncScrapValuesClientRpc(new NetworkObjectReference[] { shellObj.GetComponent<NetworkObject>() }, new int[] { shellObj.GetComponent<GrabbableObject>().scrapValue });
+                }
+            }
+        }
+
+
+        private static Item _shellItem;
+        private static Item GetShellItem()
+        {
+            if (_shellItem == null)
+            {
+                _shellItem = StartOfRound.Instance.allItemsList.itemsList.First(i => i.name == "GunAmmo");
+            }
+            return _shellItem;
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,55 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using MasksDropShells.Patches;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasksDropShells
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class MaskDropShell : BaseUnityPlugin
+    {
+        private const string modGUID = "Waffle.MasksDropShells";
+        private const string modName = "Masks Drop Shells";
+        private const string modVersion = "1.0.1";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        public static MaskDropShell Instance;
+
+        internal ManualLogSource mls;
+
+        internal ConfigEntry<int> ShellChance;
+        internal ConfigEntry<int> ShellRolls;
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            mls.LogInfo("Masks drop shells has loaded");
+
+            ShellChance = Config.Bind("General",      // The section under which the option is shown
+                                   "ShellChance",  // The key of the configuration option in the configuration file
+                                   25, // The default value
+                                   "This value controls the chance of dropping a shell for each roll, as a percentage (0-100)");
+
+            ShellRolls = Config.Bind("General",      // The section under which the option is shown
+                                   "ShellRolls",  // The key of the configuration option in the configuration file
+                                   2, // The default value
+                                   "This value controls the ammount of times it will attempt to spawn a shotgun shell, as well as the max ammount that can be spawned");
+
+            harmony.PatchAll(typeof(MaskDropShell));
+            harmony.PatchAll(typeof(MaskedPlayerEnemy_Patches));
+        }
+    }
+}
